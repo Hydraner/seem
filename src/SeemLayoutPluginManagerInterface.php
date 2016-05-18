1b4a6b7a727b8ab2de3e208f24b586f6b57ac10e
Pascal Crott <pascal.crott@erdfisch.de>
2016-05-18 15:45:20 +0200

    generate layout plugin manager.

---
+++ src/SeemLayoutPluginManagerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\seem;
+
+use Drupal\Component\Plugin\PluginManagerInterface;
+
+/**
+ * Defines an interface for seem_layout_plugin managers.
+ */
+interface SeemLayoutPluginManagerInterface extends PluginManagerInterface {
+
+    // Add getters and other public methods for seem_layout_plugin managers.
+
+}
