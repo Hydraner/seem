c9837dd5e53030a6a94487669d667d5ed62561c8
Hydra <Hydra@Hydras-MBP.fritz.box>
2016-06-15 18:03:34 +0200

    Add the rendering of the views title.

--- src/Plugin/SeemRenderable/ViewSeemRenderable.php
+++ src/Plugin/SeemRenderable/ViewSeemRenderable.php
@@ -7,10 +7,7 @@ use Drupal\seem\Plugin\SeemRenderableBase;
 use Drupal\views\Views;
 
 /**
- * Provides renderable views, defined by the layout.
- *
- * @todo:
- *   Add functionality to render the title of the view too.
+ * Provides renderable view, defined by the display.
  *
  * @SeemRenderable(
  *   id = "view",
@@ -19,26 +16,23 @@ use Drupal\views\Views;
  */
 class ViewSeemRenderable extends SeemRenderableBase {
 
-    /**
-     * {@inheritdoc}
-     */
-    public function doRenderable($content, SeemVariant $seem_variant) {
-        //return views_embed_view($content['view'], $content['display_id']);
-        $args = func_get_args();
-        $view = Views::getView($content['view']);
-        if (!$view || !$view->access($content['display_id'])) {
-            return;
-        }
-        $view->buildTitle();
-        //kint($view);
-
-        return[
-            '#title' => $view->getTitle(),
-            '#type' => 'view',
-            '#name' => $content['view'],
-            '#display_id' => $content['display_id'],
-            '#arguments' => $args,
-        ];
-
+  /**
+   * {@inheritdoc}
+   */
+  public function doRenderable($content, SeemVariant $seem_variant) {
+    $view = Views::getView($content['view']);
+    if (!$view || !$view->access($content['display_id'])) {
+      return;
     }
+
+    return [
+      // Force rendering the title, since this is what we expect it to do.
+      // @see seem_preprocess_views_view()
+      '#show_title' => TRUE,
+      '#type' => 'view',
+      '#name' => $content['name'],
+      '#display_id' => $content['display_id'],
+      '#arguments' => isset($content['arguments']) ? $content['arguments'] : [],
+    ];
+  }
 }
