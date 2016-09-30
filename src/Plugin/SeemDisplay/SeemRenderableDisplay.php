b95ff56660c6f7bc366f336d15b11143e18b946c
Pascal Crott <pascal.crott@erdfisch.de>
2016-09-30 13:47:09 +0100

    Add some clarification documentation to the seemRenderable display.

--- src/Plugin/SeemDisplay/SeemRenderableDisplay.php
+++ src/Plugin/SeemDisplay/SeemRenderableDisplay.php
@@ -3,7 +3,14 @@
 namespace Drupal\seem\Plugin\SeemDisplay;
 
 /**
- * Provides a 'NodeBlock' block plugin.
+ * Provides a 'Display' seem_renderable plugin.
+ *
+ * This is used by the 'display' seem renderable plugin to create fake display
+ * instances when nesting displays. So also if this seems to be not in use,
+ * make sure not to delete this, since it is required for the display
+ * seem_renderable to work.
+ *
+ * @see \Drupal\seem\Plugin\SeemRenderable\DisplaySeemRenderable::doRenderable
  *
  * @SeemDisplay(
  *   id = "renderarble_display",
